942303a319f0d6f53896c472780b1d9deb5b3487
Raphael <greatsharp@hotmail.com>
2019-12-25 16:00:51 +0800

    Create ElasticSimpleJob.java

---
+++ src/main/java/cn/cjw/springbootstudy/annotation/ElasticSimpleJob.java
@@ -0,0 +1,40 @@
+package cn.cjw.springbootstudy.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.core.annotation.AliasFor;
+
+/**
+ * SimpleJob任务注解
+ */
+@Inherited
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ElasticSimpleJob {
+	
+	@AliasFor("cron")
+	public abstract String value() default "";
+
+	@AliasFor("value")
+	public abstract String cron() default "";
+	
+	public abstract String jobName() default "";
+	
+	public abstract int shardingTotalCount() default 1;
+	
+	public abstract String shardingItemParameters() default "";
+
+	public abstract String jobParameter() default "";
+
+	public abstract String dataSource() default "";
+	
+	public abstract String description() default "";
+
+	public abstract boolean disabled() default false;
+
+	public abstract boolean overwrite() default true;
+}
